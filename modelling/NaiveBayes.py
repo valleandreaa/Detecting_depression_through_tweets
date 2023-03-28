ec8f60e78bd60a4faff2eddf85164e9e531e3f0d
emilrib <127395067+emilrib@users.noreply.github.com>
2023-03-28 22:27:42 +0200

    NaiveBayes.py

--- modelling/NaiveBayes.py
+++ modelling/NaiveBayes.py
@@ -1,8 +1,8 @@
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score, fbeta_score
+from sklearn.model_selection import GridSearchCV
 
-
-def multi_NB(matrix, features, data):
+def multi_NB(matrix, features, data, tuning = False):
     '''
     Support vector machine model
     :param matrix: matrix of frequency [df]
@@ -10,6 +10,9 @@ def multi_NB(matrix, features, data):
     :param data: cleaned dataset [df]
     :return:
     '''
+    param_grid = {
+    'classifier__alpha': [0.1, 0.5, 1.0, 2.0, 5.0],
+    'classifier__fit_prior': [True, False]}
 
     y_train = data[data['type']=='train']['target'].to_numpy()
     X_train = matrix[data[data['type']=='train'].index[0]:data[data['type']=='train'].index[-1]+1]
@@ -17,9 +20,16 @@ def multi_NB(matrix, features, data):
     X_test = matrix[data[data['type'] == 'develop'].index[0]:data[data['type'] == 'develop'].index[-1]+1]
     y_test = data[data['type'] == 'develop']['target'].to_numpy()
 
-    multinominal_NB = MultinomialNB
-    multinominal_NB = multinominal_NB.fit(X_train, y_train)
-    y_pred = multinominal_NB.predict(X_test)
+    multinominal_NB = MultinomialNB()
+
+    if tuning:
+        grid_search = GridSearchCV(multinominal_NB, param_grid, cv=5, n_jobs=-1)
+        grid_search.fit(X_train, y_train)
+        y_pred = grid_search.predict(X_test)
+    else:
+        multinominal_NB = multinominal_NB.fit(X_train, y_train)
+        y_pred = multinominal_NB.predict(X_test)
+
     accuracy = accuracy_score(y_test, y_pred)
     f1 = fbeta_score(y_test, y_pred, beta=1.5,average=None)
     recall = recall_score(y_test, y_pred,average=None)
