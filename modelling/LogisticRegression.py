8ffc695a8a4a80bde7d826d1beb73ac9823df0f4
Daniel Leibrock <95095674+daleibro@users.noreply.github.com>
2023-03-30 13:25:04 +0200

    Update LogisticRegression.py

--- modelling/LogisticRegression.py
+++ modelling/LogisticRegression.py
@@ -1,7 +1,34 @@
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, recall_score, precision_score, fbeta_score
 
-# metrics
-# Confusion matrix
-# Precision
-# Recall
-# f1-score
-# accuracy
+def log_reg_model(matrix, features, data):
+    '''
+    Logistic regression model
+    :param matrix: matrix of frequency [df]
+    :param features: vector of word [list]
+    :param data: cleaned dataset [df]
+    :return:
+    '''
+
+    y_train = data[data['type'] == 'train']['target'].to_numpy()
+    X_train = matrix[data[data['type'] == 'train'].index[0]:data[data['type'] == 'train'].index[-1] + 1]
+
+    X_test = matrix[data[data['type'] == 'develop'].index[0]:data[data['type'] == 'develop'].index[-1] + 1]
+    y_test = data[data['type'] == 'develop']['target'].to_numpy()
+
+    log_reg = LogisticRegression()
+    log_reg = log_reg.fit(X_train, y_train)
+    y_pred = log_reg.predict(X_test)
+
+    accuracy = accuracy_score(y_test, y_pred)
+    f1 = fbeta_score(y_test, y_pred, beta=1.5, average=None)
+    recall = recall_score(y_test, y_pred, average=None)
+    precision = precision_score(y_test, y_pred, average=None)
+
+    print(
+        'Accuracy', accuracy, '\n'
+        'F1 score', f1, '\n'
+        'recall', recall, '\n'
+        'precision', precision, '\n'
+    )
